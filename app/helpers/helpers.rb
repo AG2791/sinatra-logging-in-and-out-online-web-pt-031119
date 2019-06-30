11d71e2da448444aa2eabac5beb877bed31cff36
AJ Guiles <ashiguiles@gmail.com>
2019-06-30 01:07:28 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,6 +1,7 @@
 class Helpers
   # the following two helper methods should only be call in views to add double protection to this view so that only the current user, when they are logged in, can see their bank account balance. Helpers are also methods that make it cleaner to add logic to our views.
   
+  accept the session hash as an argument. Uses the user_id
   def current_user
   @user = User.find_by_id(session[:user_id])
     
