6852db846f7f0b60f4fc31949727711f3dca3b09
AJ Guiles <ashiguiles@gmail.com>
2019-06-30 00:43:54 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,11 @@
 class Helpers
+  # the following two helper methods should only be call in views to add double protection to this view so that only the current user, when they are logged in, can see their bank account balance. Helpers are also methods that make it cleaner to add logic to our views.
+  
+  def current_user
+end
+
+def is_logged_in?
+end
+
+
 end
